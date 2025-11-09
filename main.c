416ec90daffcfea300481bf9d06815b984b982ec
David Roman <officialdavidromanyt@gmail.com>
2025-11-09 13:51:42 -0400

    Added files

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER 1024
+
+int main() {
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    char buffer[BUFFER];
+
+    // Create socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind to all interfaces (0.0.0.0) on PORT
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    while (1) {
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+        if (client_fd < 0) {
+            perror("accept failed");
+            continue;
+        }
+
+        read(client_fd, buffer, BUFFER);
+        printf("Received request:\n%s\n", buffer);
+
+        char *response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from C server!\n";
+        write(client_fd, response, strlen(response));
+        close(client_fd);
+    }
+
+    close(server_fd);
+    return 0;
+}
